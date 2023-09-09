b057210fd5a6973f42085ac9556d65b226dcc895
Jason Armstrong <44088579+jasn-armstrng@users.noreply.github.com>
2023-09-08 20:11:07 -0400

    Update break_a_safe.py

--- break_a_safe.py
+++ break_a_safe.py
@@ -1,15 +1,13 @@
+# https://www.hackinscience.org/exercises/break-a-safe
 from itertools import permutations
-
 
 def generate_permutations(iterable: list, digits: int) -> None:
     for p in permutations(iterable, digits):
         print(', '.join(map(str, p)))
 
-
 def main() -> None:
     for i in [1, 5, 8]:
         generate_permutations([1, 5, 8, i], 4)
-
 
 if __name__ == "__main__":
     main()
