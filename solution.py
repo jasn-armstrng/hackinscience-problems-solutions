2ff08e08b4c0bc0e149e783c8248c9aea925c774
Jason Armstrong <44088579+jasn-armstrng@users.noreply.github.com>
2023-09-08 21:08:33 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,11 @@
 import sys
 
-
 def main() -> None:
     if len(sys.argv) < 2:
         print("usage: python3 solution.py PARAM")
         return
-
     params = [int(i) for i in sys.argv[1:]]
     print(sum(params))
-
 
 if __name__ == "__main__":
     main()
