fabde5e4edea6e210b0e5f93038cdcaadc51e5a8
Jason Armstrong <44088579+jasn-armstrng@users.noreply.github.com>
2023-09-08 20:51:47 -0400

    Update sum_mult_3_or_5.py

--- sum_mult_3_or_5.py
+++ sum_mult_3_or_5.py
@@ -1,11 +1,10 @@
+# https://www.hackinscience.org/exercises/multiples-of-3-and-5
 from math import floor
 
 def sum_multiples_of_3_or_5(n: int) -> int:
     x = floor((n-1)/3)
     y = floor((n-1)/5)
     z = floor((n-1)/15)
-
-    # print(x, y, z)
     return int((3*x*(x+1)/2) + (5*y*(y+1)/2) - (15*z*(z+1)/2))
 
 def main() -> None:
